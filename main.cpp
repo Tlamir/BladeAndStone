83827426ec0f48872ea4068a25d86442ae35c657
Yigithan Guven <demirejderha@gmail.com>
2025-01-03 18:40:57 +0300

    feat: Added key controls

--- main.cpp
+++ main.cpp
@@ -4,17 +4,25 @@
 #define SCREEN_HEIGHT 384
 int main() {
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Blade & Stone");
-
+    const float playerSpeed = 2.0f;
     SetTargetFPS(60); 
     //World map texture
     Texture2D worldMap = LoadTexture("Assets/WorldMap.png");
     Vector2 mapPosition = {0.0f, 0.0f};
    
-    // Main game loop
-    while (!WindowShouldClose()) { // Detect window close button or ESC key
+    
+    while (!WindowShouldClose()) 
+    {
         BeginDrawing();
         ClearBackground(RAYWHITE);
 
+        if (IsKeyDown(KEY_D))  mapPosition.x -= playerSpeed;
+        else if (IsKeyDown(KEY_A)) mapPosition.x += playerSpeed;
+        else if (IsKeyDown(KEY_W)) mapPosition.y += playerSpeed;
+        else if (IsKeyDown(KEY_S))  mapPosition.y -= playerSpeed;
+      
+        
+        
         DrawTextureEx(worldMap, mapPosition, 0, 0.651f, WHITE);
         EndDrawing();
     }
